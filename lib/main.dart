4f92d5a26f667b33fd586a70e52107ddc9f13b92
Tomasz Jaworski <tomasz.jaworski98@gmail.com>
2019-06-30 00:13:03 +0200

    Integrate with Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter_web/material.dart';
 import 'package:kakaton/LoginForm.dart';
+import 'package:firebase/firebase.dart' as firebase;
 
-void main() => runApp(MyApp());
+void main() {
+  firebase.initializeApp(
+    apiKey: "AIzaSyDkBGHnvSakkguKWj8bAXo0SCh4xBPGyZ8",
+    authDomain: "interwencja-6029c.firebaseapp.com",
+    databaseURL: "https://interwencja-6029c.firebaseio.com",
+    projectId: "interwencja-6029c",
+    storageBucket: "",
+    messagingSenderId: "1034445272799");
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
