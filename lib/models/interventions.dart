078866bde7bc729dd9055ed4ca839fc23f4de05d
Tomasz Jaworski <tomasz.jaworski98@gmail.com>
2019-06-30 03:52:33 +0200

    WIP

--- lib/models/interventions.dart
+++ lib/models/interventions.dart
@@ -1,23 +1,37 @@
+import 'dart:async';
 import 'package:firebase/firebase.dart' as firebase;
+import 'package:kakaton/models/intervention.dart';
 
 class Interventions {
-  Interventions() {
-    firebase.database().ref("interventions").onChildAdded.listen((e) {
-      print("got event!");
-      print(e.snapshot.toJson());  
+  Stream<List<Intervention>> get() {
+    var ref = firebase.database().ref("interventions");
+
+    return ref.onValue.map((queryEvent) {
+      var snapshot = queryEvent.snapshot;
+
+      return (snapshot.val() as Map<String, dynamic>).values.map((e) {
+        return Intervention(
+          dateTime: DateTime.now(),
+          contact: "test",
+          description: "test",
+          phone: "test",
+          email: "test@test.com",
+          status: "asdasd",
+          adress: "asdasd");
+      }).toList();
     });
   }
 
-  void add() async {
-    print("calling createEmptyReport...");
-
+  Future<Intervention> add() async {
     try {
       final result = 
         await firebase.functions().httpsCallable('createEmptyReport').call({});
 
-      print(result.data);
+      return null;
     } catch (e) {
       print(e);
+    
+      return null;
     }
   }
 }
